9f07f9550e881b1da42347d7c11ecbd6730da5b8
minhanh12345 <becun1003@gmail.com>
2022-05-25 20:35:48 +0700

    aasd

--- src/main/java/com/example/project_shop/repository/OrderDetailRepo.java
+++ src/main/java/com/example/project_shop/repository/OrderDetailRepo.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
+
 import java.util.List;
 
 public interface OrderDetailRepo extends JpaRepository<OrderDetailEntity,Long>, JpaSpecificationExecutor<OrderDetailEntity> {
-    @Query("select o from OrderDetailEntity o where o.order.id = ?1")
+   @Query(value = "SELECT * FROM shop.order_detail where order_id=1",nativeQuery = true)
     List<OrderDetailEntity> getAllByOrder(Long idOrder);
 }
--- src/main/java/com/example/project_shop/repository/OrderRepo.java
+++ src/main/java/com/example/project_shop/repository/OrderRepo.java
@@ -1,8 +1,12 @@
 package com.example.project_shop.repository;
 
+import com.example.project_shop.entity.OrderDetailEntity;
 import com.example.project_shop.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface OrderRepo extends JpaRepository<OrderEntity, Long>, JpaSpecificationExecutor<OrderEntity> {
+
 }
